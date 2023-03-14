5772101e44182db987fdc44e83fddc1122df9382
titofuentes17 <116034551+titofuentes17@users.noreply.github.com>
2023-03-14 17:01:57 +0100

    Update view.py

--- view.py
+++ view.py
@@ -1,25 +1,26 @@
 
+import automata_celular as ac
 from PIL import Image
 
 
 class MundoPlot:
-    def __init__(self,mundo,cell_size,color0,color1)->None:
-        self.__mundo = mundo
+    def __init__(self,mundo:ac.Mundo,cell_size,color0,color1)->None:
+        self.__mundo = mundo[0]
         self.__cell_size=cell_size
         self.__color0=color0
         self.__color1=color1
+        self.__cols= self.__mundo.getcols()
+        self.__rows = self.__mundo.getrows()
         
     def plot_estado(self,filename):
-        #num_rows, num_cols, estado, filename
+        self.__mundo
+
         cell_size = self.__cell_size
-        num_rows = self.__mundo.getrows()
-        num_cols=  self.__mundo.getcols()
-        estado= self._mundo.estado()
-        image = Image.new(mode='RGB', size=(cell_size*num_cols, cell_size*num_rows), color='white')
-        
-        for i in range(num_rows):
-            for j in range(num_cols):
-                state = estado[i*num_rows+j]
+        estado = self.__mundo.estado
+        image = Image.new(mode='RGB', size=(cell_size*self.__cols, cell_size*self.__rows), color='white')  
+        for i in range(self.__rows):
+            for j in range(self.__cols):
+                state = estado[i*self.__rows+j]
                 color = self.__color0 if state == 0 else self.__color1
                 x = j * cell_size
                 y = i * cell_size
@@ -27,5 +28,5 @@ class MundoPlot:
                     for dx in range(cell_size):
                         image.putpixel((x+dx, y+dy), color)
         image.save(filename + ".png")
+
         
-    
