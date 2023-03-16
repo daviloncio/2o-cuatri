0a8c562ed6eb14b8af75b07f7b24e8be20e59433
daviloncio <davidlv1909@gmail.com>
2023-03-16 23:37:45 +0100

    l

--- pruebas.py
+++ pruebas.py
@@ -1,48 +1,7 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+import numpy as np
 
-G = nx.Graph()
-for i in range(1, 4):
-        G.add_node(i)
-        
-G.add_edge(1, 2)
-G.add_edge(1, 3)
-G.add_edge(2, 3)   
- 
+t = np.array([1, 2, 3, 4, 5])
 
-color_map = []
-for node in G:
-     color_map.append('grey')
-    
+t=np.insert(t,(3,1,2),6)
 
-nx.draw(nx.bfs_tree(G,1), node_color=color_map, with_labels=True)
-
-
-plt.show()
-
-
-
-'''
-
-# add vertices
-for i in range(1, 4):
-        G.add_vertex(i)
-
-      
-# add edges
-
-G.set_vertices_attribute("color","white")
-print('Total edges in the graph:', G.edges_count())
-print('Total vertices in the graph:', G.vertices_count())
-print('\nPrint graph:\n', G)
-
-# DFS_iter
-DFS_iter(G, 1)
-atr = 'parent'
-print(f'\nAfter DFS_iter, Attribute {atr} of vertices:\n {G.get_vertices_attribute(atr)}')
-    
-# DFS_rec
-G.set_vertices_attribute('color', 'WHITE')
-DFS_rec(G, 1)
-atr = 'parent'
-print(f'\n After DFS_rec, attribute {atr} of vertices:\n {G.get_vertices_attribute(atr)}')'''
+print(t)
