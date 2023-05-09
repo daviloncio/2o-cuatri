57af8cb85a5a5f3e203a16015d683d213751a29a
daviloncio <davidlv1909@gmail.com>
2023-05-09 22:28:19 +0200

    gt

--- P3_EX2.pl
+++ P3_EX2.pl
@@ -8,114 +8,49 @@
     carretera(sevilla,cadiz,200).
     carretera(sevilla,huelva,100).
     carretera(sevilla,malaga,100).
+
+  
 % Reglas
 
 %redefinimos esta regla para la funcion distancia
 
 
-distancia2(Origen,Destino,X):-carretera(Origen,Destino,X).
 
 distancia(Origen,Destino,X):-carretera(Origen,Destino,X).
 distancia(Origen,Destino,X):-carretera(Destino,Origen,X).
-distancia(Origen,Destino,X):-
-    carretera(Origen,Intermedio,Y),distancia(Intermedio,Destino,Z),X is Y+Z,
-    !.
-distancia(Origen,Destino,X):
-    carretera(Intermedio,Origen,Y),distancia(Intermedio,Destino,Z),X is Y+Z.
+%si no ha funcionado ninguna de las anteriores,entonces las conectan dos carreteras
+%sea como sea,pasaremos o por cordoba o por sevilla
+distancia(Origen,Destino,X):- %caso pasamos por cordoba
+    carretera(cordoba,Origen,Y),carretera(cordoba,Destino,Z),X is Y+Z.
 
-%primero, nos damos cuneta de que para dar con todos los caminos posibles salimos de cordoba y luego de sevilla,después realizaremos
-%distancias donde sumamos kilómetros, donde Nonooooooooo siempre sumaremos carretera(sevilla,cordoba,140)
+distancia(Origen,Destino,X):-%caso pasamos por sevilla
+    carretera(sevilla,Origen,Y),carretera(sevilla,Destino,Z),X is Y+Z.
 
-conexiones_cordoba=[carretera(cordoba,granada,200),
-                    carretera(cordoba,jaen,100),
-                    carretera(cordoba,malaga,150)].
-conexiones_sevilla =[carretera(sevilla,cadiz,200),
-                    carretera(sevilla,huelva,100),
-                    carretera(sevilla,malaga,100)].
-conexion_central=carretera(cordoba,sevilla,140).
+distancia(Origen,Destino,X):-%caso pasamos por cordoba-sevilla
+    carretera(cordoba,sevilla,W),carretera(cordoba,Origen,Y),carretera(sevilla,Destino,Z),X is Y+Z+W.
 
-distancias=findall(Diccionario, 
- (carretera(Ciudad1, Ciudad2, Km),distancia(Ciudad1, Ciudad2, Distancia),
- Diccionario = _{origen:Ciudad1, destino:Ciudad2, distancia:Distancia}),
- ListaDistancias).
+distancia(Origen,Destino,X):-%caso pasamos por cordoba-sevilla
+    carretera(cordoba,sevilla,W),carretera(sevilla,Origen,Y),carretera(cordoba,Destino,Z),X is Y+Z+W.
+
+
+%A CONTINUACIÓN ESCRIBIMOS LAS REGLAS QUE NOS AYUDARÁN A HACER EL FILTRADO Y 
+%ELIMINAR DE DISTANCIAS AQUELLAS DISTANCIAS QUE 
+%TENGAN LAS MISMAS CIUDADES DE ORIGEN Y DESTINO
+
+primer_y_segundos_iguales([X,X|_]).
+
+%LA FUNCION EXCLUDE DEJA FUERA AQUELLAS LISTAS QUE CUMPLAN LA ANTERIOR REGLA.
+
+
+eliminar_iguales(Distancias, ListaFinal):- exclude(primer_y_segundos_iguales,Distancias, ListaFinal).
+
+distancias_sin_filtrado(Origen,Destino,Distancias):- 
+findall([Origen,Destino,Distancia], distancia(Origen,Destino,Distancia), Distancias).
+
+distancias_con_filtrado(Origen, Destino, ListaFinal) :-
+    distancias_sin_filtrado(Origen, Destino, Distancias),
+    eliminar_iguales(Distancias, ListaFinal),
+    write(ListaFinal).
 
 
 main :- print("inicio-carreteras").
-     :- print(distancias).
-
-
-/* Ejemplos de consultas
-
-?- main.
-"inicio-carreteras"
-true.
-
-?.- carretera(cordoba,sevilla).        
-true.
-
-?- carretera(cordoba,huelva). 
-false.
-Falso en Prolog no equivale a insatisfactible en Lógica
-
-?- conectado(sevilla,cadiz).
-true .
-
-?- conectado(cordoba,huelva). 
-true .
-
-?- carretera(almeria,almeria).
-false.
-
-?- conectado(cordoba,X).        Pulsar ; tras cada solución
-X = granada ;
-X = jaen ;
-X = malaga ;
-X = sevilla ;
-X = cadiz ;
-X = huelva ;
-X = malaga ;
-false.
-
-?- conectado(cordoba,X).  Pulsar . para no buscar más soluciones
-X = granada ;
-X = jaen ;
-X = malaga .
-
-?- conectado(cordoba,_).   
-true .
-Como en Haskell (variable anónima/comodín)
-
-?- carretera(malaga,_).
-false.
-
-?- conectado(X,X).         
-false.
-En Haskell no se permiten variables repetidas (debido a currificación)
-
-?- conectado(_,_).
-true .
-
-?- conectado(X,Y). 
-X = cordoba,
-Y = granada ;
-X = cordoba,
-Y = jaen ;
-X = cordoba,
-Y = malaga ;
-X = cordoba,
-Y = sevilla ;
-X = sevilla,
-Y = cadiz ;
-X = sevilla,
-Y = huelva ;
-X = sevilla,
-Y = malaga ;
-X = cordoba,
-Y = cadiz ;
-X = cordoba,
-Y = huelva ;
-X = cordoba,
-Y = malaga ;
-false.
-
-*/
